16e5be92186956d8c232109a62842b773b211e53
HugoSantos <condoyannis@gmail.com>
2021-08-08 21:15:20 -0300

    initial commit

---
+++ src/main/java/com/condoyannis/learn/spring/rest/LearnSpringRestCrudApplication.java
@@ -0,0 +1,13 @@
+package com.condoyannis.learn.spring.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringRestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringRestCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/condoyannis/learn/spring/rest/LearnSpringRestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.condoyannis.learn.spring.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringRestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
