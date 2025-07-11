2435f3c4486853338dfdc605d69244ebeb533622
Hugo Condoyannis <hugo.condoyannis@userede.com.br>
2025-07-11 18:03:00 -0300

    adiciona delete e post method

--- src/main/java/com/condoyannis/learn/spring/rest/controller/ClienteController.java
+++ src/main/java/com/condoyannis/learn/spring/rest/controller/ClienteController.java
@@ -1,10 +1,13 @@
 package com.condoyannis.learn.spring.rest.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,13 +25,40 @@ public class ClienteController {
 	private ClienteRepository clienteRepository;
 
 	@GetMapping
-	public List<Cliente> listar() {
+	@ResponseStatus(HttpStatus.OK)
+	public List<Cliente> listarTodos() {
 		return clienteRepository.findAll();
 	}
-
+	
+	@GetMapping("/consultar")
+	@ResponseStatus(HttpStatus.OK)
+	public List<Cliente> listarTodosPath() {
+		return listarTodos();
+	}
+	
+	@GetMapping("/{idCliente}")
+	@ResponseStatus(HttpStatus.OK)
+	public Optional<Cliente> listarCliente(@PathVariable Long idCliente) {
+		return clienteRepository.findById(idCliente);
+	}
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
+	}
+	
+	@DeleteMapping("/{idCliente}")
+	@ResponseStatus(HttpStatus.OK)
+	public void removerCliente(@PathVariable Long idCliente) {
+		Cliente cliente=new Cliente();
+		cliente.setId(idCliente);
+		clienteRepository.delete((cliente));
+	}
+	
+	@DeleteMapping                                                         
+	@ResponseStatus(HttpStatus.OK)
+	public void removerTodosCliente() {
+		clienteRepository.deleteAll();
 	}
 }
--- src/main/java/com/condoyannis/learn/spring/rest/repository/Cliente.java
+++ src/main/java/com/condoyannis/learn/spring/rest/repository/Cliente.java
@@ -7,13 +7,36 @@ import javax.persistence.Id;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 }
